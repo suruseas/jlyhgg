a5d5d32b25b6861f7d4076beea46d6fe0327ce90
moutend <moutend@gmail.com>
2016-02-19 14:26:30 +0900

    Update

--- formatt.rb
+++ formatt.rb
@@ -24,7 +24,7 @@ html_string.gsub!(MISC, '')
 html_string.gsub!(/(<[^>]*)\n/, '\1')
 html_string.gsub!(HINTBOX, 'HINTBOX \1')
 html_string.gsub!(B, '\1')
-html_string.gsub!(PRE, '```\1```')
+html_string.gsub!(PRE, "```\\1\n```")
 html_string.gsub!(ITALIC, '_\1_')
 html_string.gsub!(IMG_POSITION, '"img-\1"')
 html_string.gsub!(IMG_SIZE, '')
@@ -35,5 +35,21 @@ html_string.gsub!(H3, '### \1')
 html_string.gsub!(H4, '#### \1')
 html_string.gsub!(H1, '# \1')
 html_string.gsub!(CODE, '`\1`')
+html_string.gsub!(/([^,], |[^!]! |[^\?]\? |[^\.]\. )/, "\\1\n")
+html_string.gsub!(/ *$/, '')
+html_string.gsub!(/&gt;/, '>')
+html_string.gsub!(/&amp;/, '&')
+html_string.gsub!(/<pre[^>]*>|<\/pre>/, '```')
 
-puts html_string
+indent = false
+html_string.each_line do |line|
+  if indent
+    puts "    #{line}"
+  else
+    puts line
+  end
+
+  if line.match(/```/)
+    indent = indent ? false : true
+  end
+end
